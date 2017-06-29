13fc8ada007f0c4adab28cfbfb705caa8d716cbf
West Kraemer <WestKraemer@gmail.com>
2017-06-29 11:41:22 -0400

    add square root button

--- intro/ViewController.swift
+++ intro/ViewController.swift
@@ -32,7 +32,10 @@ class ViewController: UIViewController {
             
             switch mathematicalSymbol {
             case "∏":
-                display!.text = "3.1415926"
+                display!.text = String(Double.pi)
+            case "√":
+                let operand = Double(display!.text!)!
+                display!.text = String(sqrt(operand))
             default: break
                 
             }
